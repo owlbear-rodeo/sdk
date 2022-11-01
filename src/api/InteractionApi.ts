870cf2524f86d855ad07dcb643de2637322bf3da
Mitchell McCaffrey <mitchemmc@gmail.com>
2022-11-01 12:07:29 +1100

    Update interaction api

--- src/api/InteractionApi.ts
+++ src/api/InteractionApi.ts
@@ -1,9 +1,7 @@
 import { enablePatches, produceWithPatches } from "immer";
 import MessageBus from "../messages/MessageBus";
-import { ObjectOrArray } from "../types";
 import {
   DispatchInteractionUpdate,
-  InteractionManager,
   ItemInteraction,
   ItemInteractionManager,
   StopInteraction,
@@ -42,47 +40,6 @@ class InteractionApi {
     };
 
     return [dispatcher, stop, id];
-  }
-
-  async startCustomInteraction<T extends ObjectOrArray>(
-    baseState: T,
-  ): Promise<InteractionManager<T>> {
-    const { id } = await this.messageBus.sendAsync<{ id: string }>(
-      "OBR_INTERACTION_START_CUSTOM_INTERACTION",
-      { baseState },
-    );
-
-    let prev = baseState;
-    const dispatcher: DispatchInteractionUpdate<T> = (update) => {
-      const [next, patches] = produceWithPatches(prev, update);
-      prev = next;
-      this.messageBus.send("OBR_INTERACTION_UPDATE_CUSTOM_INTERACTION", {
-        id,
-        patches,
-      });
-      return next;
-    };
-
-    const stop: StopInteraction = () => {
-      this.messageBus.send("OBR_INTERACTION_STOP_CUSTOM_INTERACTION", { id });
-    };
-
-    return [dispatcher, stop, id];
-  }
-
-  onCustomInteraction<T extends ObjectOrArray>(
-    id: string,
-    callback: (state: T) => void,
-  ) {
-    const handleChange = (data: { state: T }) => {
-      callback(data.state);
-    };
-    this.messageBus.send("OBR_INTERACTION_CUSTOM_SUBSCRIBE", { id });
-    this.messageBus.on(`INTERACTION_${id}`, handleChange);
-    return () => {
-      this.messageBus.send("OBR_INTERACTION_CUSTOM_UNSUBSCRIBE", { id });
-      this.messageBus.off(`INTERACTION_${id}`, handleChange);
-    };
   }
 }
 
