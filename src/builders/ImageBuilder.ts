00d2d89d62655b606818402f9a1194a96edd7751
Mitchell McCaffrey <mitchemmc@gmail.com>
2023-02-03 16:14:02 +1100

    Add more options to the image builder

--- src/builders/ImageBuilder.ts
+++ src/builders/ImageBuilder.ts
@@ -1,6 +1,12 @@
 import { ImageContent } from "../types/items/ImageContent";
 import { ImageGrid } from "../types/items/ImageGrid";
-import { TextContent, TextItemType } from "../types/items/TextContent";
+import {
+  RichText,
+  TextContent,
+  TextContentType,
+  TextItemType,
+  TextSize,
+} from "../types/items/TextContent";
 import { Image } from "../types/items/Image";
 import { GenericItemBuilder } from "./GenericItemBuilder";
 import PlayerApi from "../api/PlayerApi";
@@ -52,6 +58,93 @@ export class ImageBuilder extends GenericItemBuilder<ImageBuilder> {
 
   textItemType(textItemType: TextItemType): ImageBuilder {
     this._textItemType = textItemType;
+    return this.self();
+  }
+
+  textWidth(width: TextSize): ImageBuilder {
+    this._text.width = width;
+    return this.self();
+  }
+
+  textHeight(height: TextSize): ImageBuilder {
+    this._text.height = height;
+    return this.self();
+  }
+
+  richText(richText: RichText): ImageBuilder {
+    this._text.richText = richText;
+    return this.self();
+  }
+
+  plainText(plainText: string): ImageBuilder {
+    this._text.plainText = plainText;
+    return this.self();
+  }
+
+  textType(textType: TextContentType): ImageBuilder {
+    this._text.type = textType;
+    return this.self();
+  }
+
+  textPadding(padding: number): ImageBuilder {
+    this._text.style.padding = padding;
+    return this.self();
+  }
+
+  fontFamily(fontFamily: string): ImageBuilder {
+    this._text.style.fontFamily = fontFamily;
+    return this.self();
+  }
+
+  fontSize(fontSize: number): ImageBuilder {
+    this._text.style.fontSize = fontSize;
+    return this.self();
+  }
+
+  fontWeight(fontWeight: number): ImageBuilder {
+    this._text.style.fontWeight = fontWeight;
+    return this.self();
+  }
+
+  textAlign(textAlign: "LEFT" | "CENTER" | "RIGHT"): ImageBuilder {
+    this._text.style.textAlign = textAlign;
+    return this.self();
+  }
+
+  textAlignVertical(
+    textAlignVertical: "BOTTOM" | "MIDDLE" | "TOP",
+  ): ImageBuilder {
+    this._text.style.textAlignVertical = textAlignVertical;
+    return this.self();
+  }
+
+  textFillColor(fillColor: string): ImageBuilder {
+    this._text.style.fillColor = fillColor;
+    return this.self();
+  }
+
+  textFillOpacity(fillOpacity: number): ImageBuilder {
+    this._text.style.fillOpacity = fillOpacity;
+    return this.self();
+  }
+
+  textStrokeColor(strokeColor: string): ImageBuilder {
+    this._text.style.strokeColor = strokeColor;
+    return this.self();
+  }
+
+  textStrokeOpacity(strokeOpacity: number): ImageBuilder {
+    this._text.style.strokeOpacity = strokeOpacity;
+    return this.self();
+  }
+
+  textStrokeWidth(strokeWidth: number): ImageBuilder {
+    this._text.style.strokeWidth = strokeWidth;
+    return this.self();
+  }
+
+  textLineHeight(lineHeight: number): ImageBuilder {
+    this._text.style.lineHeight = lineHeight;
     return this.self();
   }
 
