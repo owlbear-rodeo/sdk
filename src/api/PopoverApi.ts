f061de7cce5577d3f16c1595d1695ae73d5f7b5a
Mitchell McCaffrey <mitchemmc@gmail.com>
2023-07-01 16:04:21 +1000

    Add size control to popover api

--- src/api/PopoverApi.ts
+++ src/api/PopoverApi.ts
@@ -18,6 +18,28 @@ class PopoverApi {
   async close(id: string): Promise<void> {
     await this.messageBus.sendAsync("OBR_POPOVER_CLOSE", { id });
   }
+
+  async getWidth(id: string): Promise<number | undefined> {
+    const { width } = await this.messageBus.sendAsync<{
+      width: number | undefined;
+    }>("OBR_POPOVER_GET_WIDTH", { id });
+    return width;
+  }
+
+  async setWidth(id: string, width: number): Promise<void> {
+    await this.messageBus.sendAsync("OBR_POPOVER_SET_WIDTH", { id, width });
+  }
+
+  async getHeight(id: string): Promise<number | undefined> {
+    const { height } = await this.messageBus.sendAsync<{
+      height: number | undefined;
+    }>("OBR_POPOVER_GET_HEIGHT", { id });
+    return height;
+  }
+
+  async setHeight(id: string, height: number): Promise<void> {
+    await this.messageBus.sendAsync("OBR_POPOVER_SET_HEIGHT", { id, height });
+  }
 }
 
 export default PopoverApi;
