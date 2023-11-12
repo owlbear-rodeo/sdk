07678596562156a925a80ffea58c007c8c318b69
AlessandroMinali <4143332+AlessandroMinali@users.noreply.github.com>
2023-11-12 10:50:40 -0500

    remove js-base64 dep

--- src/common/getDetails.ts
+++ src/common/getDetails.ts
@@ -1,12 +1,10 @@
-import { decode } from "js-base64";
-
 export function getDetails() {
   const urlSearchParams = new URLSearchParams(window.location.search);
   const ref = urlSearchParams.get("obrref");
   let origin = "";
   let roomId = "";
   if (ref) {
-    const decodedRef = decode(ref);
+    const decodedRef = atob(ref);
     const parts = decodedRef.split(" ");
     if (parts.length === 2) {
       origin = parts[0];
