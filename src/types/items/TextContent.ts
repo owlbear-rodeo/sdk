3ebbf20eceab9b61e64cd0092ced799867796f58
Mitchell McCaffrey <mitchemmc@gmail.com>
2024-08-22 11:59:22 +1000

    Add text content types

--- src/types/items/TextContent.ts
+++ src/types/items/TextContent.ts
@@ -17,8 +17,56 @@ export interface TextStyle {
 
 export type TextContentType = "PLAIN" | "RICH";
 
-// TODO: Type rich text
-export type RichText = any[];
+export type Descendant = Element | FormattedText;
+
+export type BulletedListElement = {
+  type: "bulleted-list";
+  children: Descendant[];
+};
+
+export type NumberedListElement = {
+  type: "numbered-list";
+  children: Descendant[];
+};
+
+export type HeadingOneElement = { type: "heading-one"; children: Descendant[] };
+
+export type HeadingTwoElement = { type: "heading-two"; children: Descendant[] };
+
+export type ListItemElement = { type: "list-item"; children: Descendant[] };
+
+export type ParagraphElement = { type: "paragraph"; children: Descendant[] };
+
+export type Element =
+  | BulletedListElement
+  | NumberedListElement
+  | HeadingOneElement
+  | HeadingTwoElement
+  | ListItemElement
+  | ParagraphElement;
+
+export type ElementFormat = Element["type"];
+
+export type FormattedText = {
+  bold?: boolean;
+  italic?: boolean;
+  underline?: boolean;
+  text: string;
+};
+
+export type MarkFormat = keyof Omit<FormattedText, "text">;
+
+export type RichText = Descendant[];
+
+export function isFormattedText(
+  descendant: Descendant,
+): descendant is FormattedText {
+  return "text" in descendant;
+}
+
+export function isElement(descendant: Descendant): descendant is Element {
+  return "children" in descendant;
+}
 
 export interface TextContent {
   /** The text formatted as Slate nodes */
@@ -26,7 +74,7 @@ export interface TextContent {
   /** The text without any formatting */
   plainText: string;
   style: TextStyle;
-  /** Does this text support rich text formatting like headings. If `PLAIN` a faster text rendering path will be used. */
+  /** Which text value should be used `PLAIN` for `plainText` and `RICH` for `richText` */
   type: TextContentType;
   width: TextSize;
   height: TextSize;
